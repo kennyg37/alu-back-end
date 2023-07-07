b624aedcec3069ceb43d8bed33f37f3d99ce3ba0
kennyg37 <k.ganza@alustudent.com>
2023-07-07 11:47:22 +0200

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,39 @@
-#!/usr/bin/python3s
-""" the imported module of sys will help as take 
-command-line arguments and the request odule will help us
-access the get, post functions
+#!/usr/bin/python3
+
+""" 
+The imported module of sys will help us take command-line arguments, and the requests module will help us access the get function.
 """
+
 import sys
 import requests
 
 def get_employee_todo_progress(employee_id):
-    """ this function holds the parameter employee_id so that in case
-    we choose to use a function rather than a command-line argument
-    the code works"""
+    """ 
+    This function retrieves the TODO list progress for a given employee ID.
+    """
     response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
     if response.status_code == 200:
         todos = response.json()
         finished_tasks = []
+
         for todo in todos:
             if todo['completed'] == True:
                 finished_tasks.append(todo)
+
         employee_name = todos[0]['username']
         all_tasks = len(todos)
         finished_tasks_count = len(finished_tasks)
 
-        print(f"Employee {employee_name} is done with tasks ({completed_task_count}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks ({finished_tasks_count}/{all_tasks}):")
         for task in finished_tasks:
             print(f"\t{task['title']}")
 
     else:
-        print("no TODO list to fetch")
+        print("No TODO list to fetch.")
+
 if __name__ == "__main__":
-        if len(sys.argv) > 1:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        else:
-            print("no parameter is provided")
+    if len(sys.argv) > 1:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    else:
+        print("No parameter is provided.")

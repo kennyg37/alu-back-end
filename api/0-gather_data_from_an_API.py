6af7fae53f76e099dc782773169cdde8618a6238
kennyg37 <k.ganza@alustudent.com>
2023-07-09 12:57:45 +0200

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,29 +8,25 @@ import sys
 """Imported modules to be used by the program"""
 
 __author__ = "Junior"
-
 if __name__ == '__main__':
     """Scripts to be executed"""
     user_id = sys.argv[1]
     response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    )
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
     data = json.loads(response.text)
     todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/'
-    )
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id))
     result = json.loads(todos.text)
     done = 0
     total = 0
-    item_arr = []
+    itemArr = []
 
     for item in result:
-        if data['id'] == item['userId']:
-            total += 1
-            if item['completed']:
-                done += 1
-                item_arr.append(item['title'])
+        total += 1
+        if item['completed']:
+            done += 1
+            itemArr.append(item['title'])
 
-    print("Employee {} is done with ({}/{})".format(data['name'], done, total))
-    for item in item_arr:
-        print("\t{}".format(item))
+    print("Employee {} is done with ({}/{}) tasks".format(data['name'], done, total))
+    for item in itemArr:
+        print("\t {}".format(item))

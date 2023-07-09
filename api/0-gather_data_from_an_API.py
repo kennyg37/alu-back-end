07293c840652d7d29c5d4ead57087df4af271b8c
kennyg37 <k.ganza@alustudent.com>
2023-07-09 13:00:26 +0200

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
             done += 1
             itemArr.append(item['title'])
 
-    print("Employee {} is done with ({}/{}) tasks".format(data['name'], done, total))
+    print("Employee {} is done with ({}/{}) tasks"
+            .format(data['name'], done, total))
     for item in itemArr:
         print("\t {}".format(item))
